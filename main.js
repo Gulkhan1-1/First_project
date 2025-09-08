48200d5d9f3fdcf6c54dd35895f08570abfeff6a
Gulkhan wz <gkhanwz@gmail.com>
2025-09-08 19:33:56 +0430

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var btn=document.querySelector('.btn');
+var text=document.querySelector('.sidebar');
+var n=document.querySelector('.span-text');
+
+btn.addEventListener('click', function(){
+    text.classList.toggle('hide');
+});
